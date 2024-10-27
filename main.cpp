e6e019dea88502ac4a3a5bab1e8c57712aa0b578
2154401527@qq.com <>
2024-10-27 20:22:01 +0800

    完成作业

--- main.cpp
+++ main.cpp
@@ -10,11 +10,41 @@ struct Result
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
-    int arr[] = { a, b, c };
-    std::sort(arr, arr + 3);
+    if (a <= b && a <= c) {
+        result.min = a;
+    }
+    else if (b <= a && b <= c) {
+        result.min = b;
+    }
+    else {
+        result.min = c;
+    }
 
    
-    result.min = arr[0];
-    result.mid = arr[1];
-    result.max = arr[2];
-}
+    if (a >= b && a >= c) {
+        result.max = a;
+    }
+    else if (b >= a && b >= c) {
+        result.max = b;
+    }
+    else {
+        result.max = c;
+    }
+
+    
+    if ((a >= b && a <= c) || (a <= b && a >= c)) {
+        result.mid = a;
+    }
+    else if ((b >= a && b <= c) || (b <= a && b >= c)) {
+        result.mid = b;
+    }
+    else {
+        result.mid = c;
+    }
+
+    }
+
+   
+   
+   
+    
